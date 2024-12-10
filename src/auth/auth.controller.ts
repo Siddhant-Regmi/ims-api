9c1fc40aae23dcd4b6f03772750c47fa9a517df6
Siddhant Regmi <siddhantregmi121@gmail.com>
2024-12-10 16:53:55 +0545

    Added public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Public } from 'src/helpers/public';
 
 export class RegisterDto extends CreateUserDto {}
 export class LoginDto {
@@ -17,6 +18,7 @@ export class LoginDto {
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Public()
   @Post('register')
   async register(@Body() registerDto: RegisterDto){
     return this.authService.register(registerDto);
